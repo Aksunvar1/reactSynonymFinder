69c84bebe0f83f7afec2336f2e6690ba618676aa
Burhan Aras <burhanaras@Burhan-Air.bbrouter>
2022-11-06 12:46:44 +0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import './App.css'
+import {useGetSynonyms} from "./hooks/useGetSynonyms";
+
+function App() {
+    const [word, setWord] = useState("")
+    const {isLoading, synonyms, getSynonyms} = useGetSynonyms()
+
+    const handleFetchSynonyms = (e: React.FormEvent) => {
+        e.preventDefault();
+        getSynonyms(word);
+    }
+
+    function handleSynonymClicked(newWord: string) {
+        setWord(newWord);
+        getSynonyms(newWord);
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={handleFetchSynonyms}>
+                <label htmlFor="word-input">Your Word</label>
+                <input value={word} onChange={(e) => setWord(e.target.value)} id="word-input"/>
+                <button>Submit</button>
+            </form>
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : (
+                <ul>
+                    {synonyms.map((synonym) => (
+                        <li
+                            onClick={() => handleSynonymClicked(synonym.word)}
+                            key={synonym.score}>
+                            {synonym.word}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+export default App
